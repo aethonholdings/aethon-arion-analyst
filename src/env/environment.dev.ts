a4be4896a816e191e58d40c6fe3da9042e32068c
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-08-23 11:42:39 +0100

    Fixed bug in C1 configurator component input field to make it a single numerical input as opposed to array

--- src/env/environment.dev.ts
+++ src/env/environment.dev.ts
@@ -1,8 +1,7 @@
 export const environment = {
     debug: true,
     host: {
-        hostname: "http://localhost",
-        path: "arion",
-        port: 3000
+        hostname: "http://asterix.local",
+        path: "arion"
     }
 };
